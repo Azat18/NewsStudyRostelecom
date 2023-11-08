e5233206d479b98a81a6428308d351464cb929b4
Azat18 <azfreedom@yandex.ru>
2023-11-08 15:05:55 +0500

    Первый коммит. Сделано меню и адаптирован шаблон

---
+++ NewsStudyRostelecom/main/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import News
+
+def index(request):
+    value = 100
+    n1 = News('Новость 1', 'Текст 1', '05.05.2025')
+    n2 = News('Новость 2', 'Текст 2', '10.05.2025')
+    l = [n1, n2]
+    context = {'title': 'Страница главная',
+               'Header1': 'Заголовок страницы',
+               'value': value,
+               'numbers': l}
+    return render(request, 'main/index.html',context)
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def content(request):
+    return render(request, 'main/content.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+def sidebar(request):
+    return render(request, 'main/sidebar.html')
