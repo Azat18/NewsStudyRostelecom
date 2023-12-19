495dcf73acb509eba0e1de479b521ead96bf1071
Azat18 <AZFreedom@yandex.ru>
2023-12-19 10:05:03 +0500

    Merge remote-tracking branch 'origin/master'

--- NewsStudyRostelecom/NewsStudyRostelecom/settings.py
+++ NewsStudyRostelecom/NewsStudyRostelecom/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 import os, inspect
 import os
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,17 +92,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'NewsStudyRostelecom.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': env("DB_NAME"),
+       'USER': env("DB_USER"),
+       'PASSWORD': env("DB_PASSWORD"),
+       'HOST': env("DB_HOST"),
+       'PORT': env("DB_PORT")
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
